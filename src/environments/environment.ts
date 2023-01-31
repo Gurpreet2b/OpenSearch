c0f75486f4888dac0eabb7228166dc7e09e6f582
Gurpreet Singh <gurpreets@2binnovations.com>
2023-01-31 15:56:46 +0530

    report mail pdf and sidebar signin component changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   // apiUrl: 'http://localhost:8000'
   // apiUrl: 'http://2bofficedubai.ddns.net:7445'
-  apiUrl: 'http://192.168.3.178:7445'
+  apiUrl: 'http://192.168.3.80:7445'
 
  
 };
